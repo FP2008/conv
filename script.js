b7f1a7c137d154a73b756bbbe58aaa996022f3a6
François Poirrier <46083896+FP2008@users.noreply.github.com>
2021-11-04 23:05:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-document.getElementById("cc").addEventListener("input",convPoids);
-document.getElementById("cs").addEventListener("input",convPoids);
-document.getElementById("ml").addEventListener("input",convPoids);
-document.getElementById("cl").addEventListener("input",convPoids);
+document.getElementById("li").addEventListener("input",convPoids);
 document.getElementById("dl").addEventListener("input",convPoids);
+document.getElementById("cl").addEventListener("input",convPoids);
+document.getElementById("ml").addEventListener("input",convPoids);
+document.getElementById("cs").addEventListener("input",convPoids);
+document.getElementById("cc").addEventListener("input",convPoids);
 
 function round(value, step) {
   step || (step = 1.0);
@@ -16,39 +17,52 @@ function convPoids(e){
   const id = e.target.id;
   const valeur = parseFloat(e.target.value);
   if(valeur === NaN)return;
-  if(id == "ml"){
-    ml.value = round(valeur, 1.0);
-    cl.value = round(valeur / 10, 0.1);
-    dl.value = round(valeur / 100, 0.01);
-    cc.value = round(valeur / 5, 0.5);
-    cs.value = round(valeur / 15, 0.5);
-  }
-  else if(id == "cl"){
-    cl.value = round(valeur, 1.0);
-    ml.value = round(valeur * 10, 1.0);
-    dl.value = round(valeur / 10, 0.1);
-    cc.value = round(valeur / 0.5, 0.5);
-    cs.value = round(valeur / 1.5, 0.5);
+  if(id == "li"){
+    li.value = round(valeur, 1.0);
+    dl.value = round(valeur * 10, 1.0);
+    cl.value = round(valeur * 100, 1.0);
+    ml.value = round(valeur * 1000, 1.0);
+    cs.value = round(valeur / 0.015, 0.5);
+    cc.value = round(valeur / 0.005, 0.5);
   }
   else if(id == "dl"){
     dl.value = round(valeur, 1.0);
+    li.value = round(valeur /10, 0.1)
+    cl.value = round(valeur * 10, 1.0);
     ml.value = round(valeur * 100, 1.0);
-    cl.value = round(valeur * 10, 0.1);
-    cc.value = round(valeur / 0.05, 0.05);
     cs.value = round(valeur / 0.15, 0.5);
+    cc.value = round(valeur / 0.05, 0.05);
   }
-  else if(id == "cc"){
-    cc.value = round(valeur, 1.0);
-    ml.value = round(valeur * 5, 1.0);
-    cl.value = round(valeur * 0.5, 0.1);
-    dl.value = round(valeur * 0.05, 0.01);
-    cs.value = round(valeur / 3, 0.5);
+    else if(id == "cl"){
+    cl.value = round(valeur, 1.0);
+    li.value = round(valeur /100, 0.01)
+    dl.value = round(valeur / 10, 0.1);
+    ml.value = round(valeur * 10, 1.0);  
+    cs.value = round(valeur / 1.5, 0.5);
+    cc.value = round(valeur / 0.5, 0.5);
+  }
+  if(id == "ml"){
+    ml.value = round(valeur, 1.0);
+    li.value = round(valeur /1000, 0.001)
+    dl.value = round(valeur / 100, 0.01);
+    cl.value = round(valeur / 10, 0.1);
+    cs.value = round(valeur / 15, 0.5);
+    cc.value = round(valeur / 5, 0.5);
   }
   else if(id == "cs"){
     cs.value = round(valeur, 1.0);
-    ml.value = round(valeur * 15, 1.0);
-    cl.value = round(valeur * 1.5, 0.1);
+    li.value = round(valeur * 0.015, 0.001);
     dl.value = round(valeur * 0.15, 0.01);
+    cl.value = round(valeur * 1.5, 0.1);
+    ml.value = round(valeur * 15, 1.0);
     cc.value = round(valeur * 3, 0.5);     
+  }
+  else if(id == "cc"){
+    cc.value = round(valeur, 1.0);
+    li.value = round(valeur * 0.005, 0.001);
+    dl.value = round(valeur * 0.05, 0.01);
+    cl.value = round(valeur * 0.5, 0.1);
+    ml.value = round(valeur * 5, 1.0);
+    cs.value = round(valeur / 3, 0.5);
   }
 }
